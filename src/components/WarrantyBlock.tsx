b9e5d14859a85a634663d7c972f5bfee72a0e215
Joz Favreau <3405708+Showcase-Joz@users.noreply.github.com>
2026-04-21 12:56:32 +0100

    change warrantyText to height mode

--- src/components/WarrantyBlock.tsx
+++ src/components/WarrantyBlock.tsx
@@ -677,7 +677,7 @@ const WarrantyBlock = ({
             chars={undefined}
             textfit
             textfitConfig={{
-              maxHeight: "parent",
+              maxHeight: "self",
               fontUnit: "em",
             }}
           ></TextElement>
