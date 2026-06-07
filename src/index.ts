d12ea7a8672381b3ffce563e5282e78a57fc6fa6
Joz Favreau <3405708+Showcase-Joz@users.noreply.github.com>
2026-06-07 20:44:47 +0100

    reorder index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export { WarrantyBlock } from "./components/WarrantyBlock.js";
 export { AnnouncementBanner } from "./components/AnnouncementBanner.js";
+export { OfferBlock } from "./components/OfferBlock.js";
+export { ButtonCTA } from "./components/ButtonCTA.js";
+export { LeaseOfferBlock } from "./components/LeaseOfferBlock.js";
 export type {
   WarrantyBlockFallbackContent,
   WarrantyBlockField,
@@ -11,19 +14,16 @@ export type {
   AnnouncementBannerField,
   AnnouncementBannerProps,
 } from "./components/AnnouncementBanner.js";
-export { OfferBlock } from "./components/OfferBlock.js";
 export type {
   OfferBlockFallbackContent,
   OfferBlockField,
   OfferBlockProps,
 } from "./components/OfferBlock.js";
-export { ButtonCTA } from "./components/ButtonCTA.js";
 export type {
   ButtonCTAFallbackContent,
   ButtonCTAField,
   ButtonCTAProps,
 } from "./components/ButtonCTA.js";
-export { LeaseOfferBlock } from "./components/LeaseOfferBlock.js";
 export type {
   LeaseOfferBlockFallbackContent,
   LeaseOfferBlockField,
