770b99eafdcb0e2119060b5ce99964311b1ec9b9
Joz Favreau <3405708+Showcase-Joz@users.noreply.github.com>
2026-08-16 19:43:05 +0100

    alter the offerOption SQUARE ratio to 1.6

--- src/components/OfferOptionBlock.tsx
+++ src/components/OfferOptionBlock.tsx
@@ -467,7 +467,7 @@ const OfferOptionBlockWrapper = styled.div<{}>`
       }
     }
   }
-  @container offerOptionBlock (max-aspect-ratio: 1.5 / 1) {
+  @container offerOptionBlock (max-aspect-ratio: 1.6 / 1) {
     .offerOptionBlockWrapper {
       --offerOptionBlockWrapperPadding: 2em;
 
