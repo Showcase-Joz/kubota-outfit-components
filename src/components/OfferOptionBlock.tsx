7290c7fb3be11e35e25855f7a8959b59ae35002c
Joz Favreau <3405708+Showcase-Joz@users.noreply.github.com>
2026-09-17 13:22:55 +0100

    test postText size

--- src/components/OfferOptionBlock.tsx
+++ src/components/OfferOptionBlock.tsx
@@ -698,7 +698,7 @@ const OfferOptionBlock = ({
     fontUnit: "cqi",
   };
   const landscapeSmallTextfitConfig = {
-    minFontSize: 2.5,
+    minFontSize: 2.8,
     maxFontSize: 3.6,
     heightOnly: true,
     fontUnit: "cqi",
