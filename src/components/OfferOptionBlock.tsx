7b6a0633d23ad959aa75fc954fb94cb8866a1616
Joz Favreau <3405708+Showcase-Joz@users.noreply.github.com>
2026-09-17 14:05:46 +0100

    test postText size

--- src/components/OfferOptionBlock.tsx
+++ src/components/OfferOptionBlock.tsx
@@ -699,7 +699,7 @@ const OfferOptionBlock = ({
   };
   const landscapeSmallTextfitConfig = {
     minFontSize: 2.6,
-    maxFontSize: 3.6,
+    maxFontSize: 3.4,
     heightOnly: true,
     fontUnit: "cqi",
   };
@@ -941,7 +941,7 @@ const OfferOptionBlock = ({
                   ? squareSmallTextfitConfig
                   : isSquare
                   ? squareTextfitConfig
-                  : wrapperWidth && wrapperWidth < 300
+                  : wrapperWidth && wrapperWidth <= 300
                   ? landscapeSmallTextfitConfig
                   : landscapeTextfitConfig
               }
